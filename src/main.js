7372df4e98fd8fefee19c11d10891501204798d6
linksprod <cto@linksprod.com>
2023-09-05 17:17:08 +0200

    initial commit of main page
    installing vutify

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import { createApp } from 'vue'
+import "./assets/css/globals.css" 
+import "./assets/css/style-guide.css"
 import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
 
-loadFonts()
-
-createApp(App)
-  .use(router)
-  .use(vuetify)
-  .mount('#app')
+createApp(App).mount('#app')
