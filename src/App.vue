27e70155c1b2313e8b2d69605909e11053b2a303
linksprod <cto@linksprod.com>
2023-09-06 00:08:29 +0200

    draft

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,85 @@
 <template>
   
+  <v-app>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title">
+          <v-list-tile-action>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app>
+      <span class="hidden-sm-and-up">
+        <v-toolbar-side-icon @click="sidebar = !sidebar">
+        </v-toolbar-side-icon>
+      </span>
+      <v-toolbar-title>
+        <img :src="sidebar_logo" class="main_logo"/>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          {{ item.title }}
+        </v-btn>
+        <v-btn class="master-primary-button" to="/reservation">예약</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    
+    <v-content>
+    </v-content>
+    
+  </v-app>
+
 </template>
 
 <script>
-  export default {
-    name: "App",
-    
-    data() {
-      return {  };
-    },
-  };
+//import HelloWorld from "./components/HelloWorld";
 
-  
+export default {
+  name: "App",
+  data(){
+    return {
+      appTitle: 'Awesome App',
+      sidebar: false,
+      sidebar_logo: './assets/img/main_logo.png', 
+      menuItems: [
+          { title: '튀니지에 관한 정보', path: '/information_about_tunisia'},
+          { title: '관광 정보', path: '/tourist_information'},
+          { title: '여행하다', path: '/travel'},
+          { title:'블로그', path: '/blog'}
+     ]
+    }
+  },
+};
 </script>
+<style>
+.main_logo {
+  width: 47px;
+  height: 50px;
+  flex-shrink: 0;
+}
+
+.master-primary-button{
+  display: flex;
+width: 77px;
+height: 39px;
+padding: 18px 24px;
+justify-content: flex-end;
+align-items: center;
+gap: 8px;
+flex-shrink: 0;
+border-radius: 12px;
+border: 1px solid #000;
+background: #3BAEFF;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
-import "./assets/css/globals.css" 
-import "./assets/css/style-guide.css"
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
