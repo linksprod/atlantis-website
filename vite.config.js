11dd9e20ac3d26f747f6cb366ca1e7abe5bac20b
Nourhene <nourhene.bakalti@esprit.tn>
2024-10-09 21:46:29 +0100

    fx 3

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: 'https://tunisiatrip.com/', // Set the base URL for assets
+    base: 'https://tunisiatrip.com/', // hedy ent localhost ?Set the base URL for assets
     plugins: [
         laravel({
             input: [
